6d4249fc6435cb96684be7fc3473d3ce663538e7
Billionaire <billionaire@billioniresimac.hitronhub.home>
2018-07-02 10:17:20 -0600

    Initial Commit

---
+++ GerardusHermens_MMExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GerardusHermens_MMExercise
+//
+//  Created by Billionaire on 2018-07-02.
+//  Copyright © 2018 GHermens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
